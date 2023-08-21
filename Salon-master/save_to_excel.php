69c88446636a78d198a8ec4615a3f4ef99932f53
s0163614 <90596569+s0163614@users.noreply.github.com>
2023-08-21 15:04:24 +0300

    Update save_to_excel.php

--- Salon-master/save_to_excel.php
+++ Salon-master/save_to_excel.php
@@ -1,18 +1,25 @@
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
+// несколько получателей
+$to  = 'sirodzhovss@mail.ru' . ', ';  // обратите внимание на запятую
 
-  $to = "sirodzhovss@mail.ru";
-  $subject = "Новое сообщение с сайта";
-  $headers = "From: " . $name . "<" . $email . ">";
-  $body = $message;
 
-  if(mail($to, $subject, $body, $headers)){
-    echo "Сообщение успешно отправлено!";
-  } else{
-    echo "Ошибка при отправке сообщения.";
-  }
-}
+// тема письма
+$subject = 'Письмо с моего сайта';
+
+// текст письма
+$message = 'Пользователь' . $_POST['name'] . ' отправил вам письмо:<br />' . $_POST['message'] . '<br />
+Связяться с ним можно по email <a href="mailto:' . $_POST['email'] . '">' . $_POST['email'] . '</a>'
+;
+
+// Для отправки HTML-письма должен быть установлен заголовок Content-type
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+
+// Дополнительные заголовки
+$headers .= 'To: Иван <sirodjov33@gmail.com>' . "\r\n"; // Свое имя и email
+$headers .= 'From: '  . $_POST['name'] . '<' . $_POST['email'] . '>' . "\r\n";
+
+
+// Отправляем
+mail($to, $subject, $message, $headers);
 ?>
